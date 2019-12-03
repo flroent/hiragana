9efbeb975e5be47a98286f4ec93b35301c15bb0b
John Bobby <johnbobby@iMac-de-John.local>
2019-12-03 12:42:26 +0100

    refactorisation hiraganatype

--- src/components/HiraganaType.jsx
+++ src/components/HiraganaType.jsx
@@ -26,11 +26,11 @@ const styles = {
   },
 };
 
-const buttonGroups = {
-  row1: ['a', 'k', 's', 't', 'n'],
-  row2: ['h', 'm', 'r', 'ywn'],
-  row3: ['dakutens', 'composes'],
-};
+const buttonGroups = [
+  ['a', 'k', 's', 't', 'n'],
+  ['h', 'm', 'r', 'ywn'],
+  ['dakutens', 'composes'],
+];
 
 const HiraganaType = ({ onCategorieClick, categories }) => (
   <Container style={styles.container}>
@@ -41,42 +41,20 @@ const HiraganaType = ({ onCategorieClick, categories }) => (
       <p>Choose categories</p>
     </Row>
     <Row style={styles.row}>
-      <ButtonGroup style={styles.buttonGroup}>
-        {buttonGroups.row1.map((categorie) => (
-          <Button
-            variant={categories.includes(categorie) ? 'danger' : 'primary'}
-            style={styles.button}
-            key={categorie}
-            onClick={onCategorieClick(categorie)}
-          >
-            {categorie.toUpperCase()}
-          </Button>
-        ))}
-      </ButtonGroup>
-      <ButtonGroup style={styles.buttonGroup}>
-        {buttonGroups.row2.map((categorie) => (
-          <Button
-            variant={categories.includes(categorie) ? 'danger' : 'primary'}
-            style={styles.button}
-            key={categorie}
-            onClick={onCategorieClick(categorie)}
-          >
-            {categorie.toUpperCase()}
-          </Button>
-        ))}
-      </ButtonGroup>
-      <ButtonGroup style={styles.buttonGroup}>
-        {buttonGroups.row3.map((categorie) => (
-          <Button
-            variant={categories.includes(categorie) ? 'danger' : 'primary'}
-            style={styles.button}
-            key={categorie}
-            onClick={onCategorieClick(categorie)}
-          >
-            {categorie.toUpperCase()}
-          </Button>
-        ))}
-      </ButtonGroup>
+      {buttonGroups.map((row) => (
+        <ButtonGroup style={styles.buttonGroup} key={row}>
+          {row.map((categorie) => (
+            <Button
+              variant={categories.includes(categorie) ? 'danger' : 'primary'}
+              style={styles.button}
+              key={categorie}
+              onClick={onCategorieClick(categorie)}
+            >
+              {categorie.toUpperCase()}
+            </Button>
+          ))}
+        </ButtonGroup>
+      ))}
     </Row>
     <Row style={styles.row}>
       <Button size="lg">All</Button>
